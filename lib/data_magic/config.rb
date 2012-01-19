b1c5278529a2e2f6537b7415411ee0b724864bb8
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-01-19 17:47:16 -0500

    getting under way

--- lib/data_magic/config.rb
+++ lib/data_magic/config.rb
@@ -3,10 +3,23 @@ module DataMagic
 
     class << self
       attr_accessor :data_magic_yml_directory
-      
+
+      #
+      # retrieve the yml_directory - the location where all of the yml
+      # files will be located.
+      #
       def yml_directory
         @data_magic_yml_directory ||= 'config'
       end
+
+      #
+      # set the yml_directory - this is where the gem will look for
+      # all of the yml files.
+      #
+      def yml_directory=(value)
+        @data_magic_yml_directory = value
+      end
+      
     end
     
   end
--- spec/lib/data_magic_spec.rb
+++ spec/lib/data_magic_spec.rb
@@ -2,8 +2,13 @@ require 'spec_helper'
 
 describe DataMagic do
   context "configuring the yml directory" do
-    it "default to a directory named config" do
+    it "should default to a directory named config" do
       DataMagic::Config.yml_directory.should == 'config'
+    end
+
+    it "should store a yml directory" do
+      DataMagic::Config.yml_directory = 'test_dir'
+      DataMagic::Config.yml_directory.should == 'test_dir'
     end
   end
 end
