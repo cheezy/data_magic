18de69ec88da02b36b90fa7dee2290ea99bd8741
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-01-20 07:31:45 -0500

    started to add the evals

--- lib/data_magic.rb
+++ lib/data_magic.rb
@@ -2,9 +2,40 @@ require "data_magic/version"
 require "data_magic/config"
 require "data_magic/reader"
 
+require 'faker'
+
 module DataMagic
 
+  def data_for(key)
+    data = DataMagic.yml[key]
+    prep_data data
+  end
+
+  private
+
+  def prep_data(data)
+    data.each { |key, value| data[key] = eval(value) }
+    data
+  end
+
+  def name
+    Faker::Name.name
+  end
+
+  def first_name
+    Faker::Name.first_name
+  end
+
+  def last_name
+    Faker::Name.last_name
+  end
+
   class << self
+    attr_reader :yml
+    
+    #
+    # load the provided filename from the config directory
+    #
     def load(filename)
       @yml = reader.load_file(filename)
     end
--- spec/lib/data_magic_spec.rb
+++ spec/lib/data_magic_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
+class TestSubject
+  include DataMagic
+end
+
 describe DataMagic do
-  context "configuring the yml directory" do
+  context "when configuring the yml directory" do
     it "should default to a directory named config" do
       DataMagic::Config.yml_directory.should == 'config'
     end
@@ -12,14 +16,42 @@ describe DataMagic do
     end
   end
 
-  context "reading yml files" do
-    before(:each) do
-      DataMagic::Config.yml_directory = 'test'
-    end
-    
+  context "when reading yml files" do
     it "should read files from the config directory" do
+      DataMagic::Config.yml_directory = 'test'
       YAML.should_receive(:load_file).with("test/fname").and_return({})
       DataMagic.load("fname")
+    end
+  end
+
+  context "when delivering data" do
+    let(:example) { TestSubject.new }
+
+    def setup_data(data)
+      DataMagic.should_receive(:yml).and_return(data)
+    end
+    
+    it "should deliver the hash from the yaml" do
+      setup_data({'key' => {'field' => 'value'}})
+      example.data_for('key').should == {'field' => 'value'}
+    end
+
+    it "should add a name" do
+      Faker::Name.should_receive(:name).and_return('Joseph')
+      setup_data({'key' => {'field' => 'name'}})
+      example.data_for('key').should == {'field' => 'Joseph'}
+    end
+
+    it "should add first name" do
+      Faker::Name.should_receive(:first_name).and_return('Sam')
+      setup_data({'key' => {'field' => 'first_name'}})
+      example.data_for('key').should == {'field' => 'Sam'}
+    end
+
+    it "should add last name" do
+      Faker::Name.should_receive(:last_name).and_return('Smith')
+      setup_data({'key' => {'field' => 'last_name'}})
+      example.data_for('key').should == {'field' => 'Smith'}
     end
   end
 end
