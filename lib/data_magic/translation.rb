34e29c21af29d36109e7927f6b451794aa80d5dc
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-01-20 09:12:25 -0500

    have basic address evals complete

--- lib/data_magic/translation.rb
+++ lib/data_magic/translation.rb
@@ -1,16 +1,66 @@
 module DataMagic
   module Translation
+    #
+    # return a random name (first and last)
+    #
     def name
       Faker::Name.name
     end
 
+    #
+    # return a random first name
+    #
     def first_name
       Faker::Name.first_name
     end
 
+    #
+    # return a random last name
+    #
     def last_name
       Faker::Name.last_name
     end
 
+    #
+    # return a random street address
+    #
+    def street_address
+      Faker::Address.street_address
+    end
+
+    #
+    # return a random city
+    #
+    def city
+      Faker::Address.city
+    end
+
+    #
+    # return a random state
+    #
+    def state
+      Faker::Address.state
+    end
+
+    #
+    # return a random state abbreviation
+    #
+    def state_abbr
+      Faker::Address.state_abbr
+    end
+
+    #
+    # return a random zip code
+    #
+    def zip_code
+      Faker::Address.zip_code
+    end
+
+    #
+    # return a random country
+    #
+    def country
+      Faker::Address.country
+    end
   end
 end
--- spec/lib/data_magic_spec.rb
+++ spec/lib/data_magic_spec.rb
@@ -36,22 +36,62 @@ describe DataMagic do
       example.data_for('key').should == {'field' => 'value'}
     end
 
-    it "should add a name" do
-      Faker::Name.should_receive(:name).and_return('Joseph')
-      setup_data({'key' => {'field' => 'name'}})
-      example.data_for('key').should == {'field' => 'Joseph'}
+    context "translating random names" do
+      it "should add a name" do
+        Faker::Name.should_receive(:name).and_return('Joseph')
+        setup_data({'key' => {'field' => 'name'}})
+        example.data_for('key').should == {'field' => 'Joseph'}
+      end
+
+      it "should add first name" do
+        Faker::Name.should_receive(:first_name).and_return('Sam')
+        setup_data({'key' => {'field' => 'first_name'}})
+        example.data_for('key').should == {'field' => 'Sam'}
+      end
+
+      it "should add last name" do
+        Faker::Name.should_receive(:last_name).and_return('Smith')
+        setup_data({'key' => {'field' => 'last_name'}})
+        example.data_for('key').should == {'field' => 'Smith'}
+      end
     end
 
-    it "should add first name" do
-      Faker::Name.should_receive(:first_name).and_return('Sam')
-      setup_data({'key' => {'field' => 'first_name'}})
-      example.data_for('key').should == {'field' => 'Sam'}
-    end
+    context "translating random addresses" do
+      it "should add a street address" do
+        Faker::Address.should_receive(:street_address).and_return("123 Main")
+        setup_data({'key' => {'field' => 'street_address'}})
+        example.data_for('key').should == {'field' => '123 Main'}
+      end
 
-    it "should add last name" do
-      Faker::Name.should_receive(:last_name).and_return('Smith')
-      setup_data({'key' => {'field' => 'last_name'}})
-      example.data_for('key').should == {'field' => 'Smith'}
+      it "should add a city" do
+        Faker::Address.should_receive(:city).and_return('Cleveland')
+        setup_data({'key' => {'field' => 'city'}})
+        example.data_for('key').should == {'field' => 'Cleveland'}
+      end
+
+      it "should add a state" do
+        Faker::Address.should_receive(:state).and_return('Ohio')
+        setup_data({'key' => {'field' => 'state'}})
+        example.data_for('key').should == {'field' => 'Ohio'}
+      end
+
+      it "should add a state abbreviation" do
+        Faker::Address.should_receive(:state_abbr).and_return('OH')
+        setup_data({'key' => {'field' => 'state_abbr'}})
+        example.data_for('key').should == {'field' => 'OH'}
+      end
+
+      it "should add a zip code" do
+        Faker::Address.should_receive(:zip_code).and_return('11111')
+        setup_data({'key' => {'field' => 'zip_code'}})
+        example.data_for('key').should == {'field' => '11111'}
+      end
+
+      it "should add a country" do
+        Faker::Address.should_receive(:country).and_return("United States")
+        setup_data({'key' => {'field' => 'country'}})
+        example.data_for('key').should == {'field' => 'United States'}
+      end
     end
   end
 end
