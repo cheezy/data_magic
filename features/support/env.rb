26d6d4611f1281dc6329b059867490ffd9506d7d
vveliev <vveliev@ryerson.ca>
2015-09-04 01:20:57 -0400

    remove debugger

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
 
 require 'rspec/expectations'
 require 'data_magic'
-require 'debugger'
 
 Before do
   DataMagic.yml_directory = nil
