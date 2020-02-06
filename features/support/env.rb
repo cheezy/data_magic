49beab536fab6d95c0baf8e8357918632901daea
jacob-ewald <jacob.ewald@gmail.com>
2020-02-06 18:37:28 -0500

    Need to specify a locale

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,8 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
 require 'rspec/expectations'
 require 'data_magic'
 
+DataMagic.locale = 'en-US'
+
 Before do
   DataMagic.yml_directory = nil
   DataMagic.yml = nil
